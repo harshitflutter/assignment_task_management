355ba5b48f10676c4805ba1fd8cfc72aea28c19e
IMac-User <harshit.flutter@gmail.com>
2025-10-28 14:56:36 +0530

    Bug Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:task_management/app.dart';
 import 'package:task_management/firebase_options.dart';
 import 'package:task_management/src/core/services/local_storage_service/hive_storage_service.dart';
@@ -8,5 +9,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await HiveStorageService.instance.init();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
